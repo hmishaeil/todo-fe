98217789fbb39ebfa92463f65c96144dced267ab
hmishaeil <hmishaeil@gmail.com>
2021-05-25 14:48:52 -0700

    screenshots added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert';
 
 
 @Component({
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  images = [
+    { url: "../../../assets/signup.png", alt: "SignUp Form" },
+    { url: "../../../assets/login.png", alt: "Login Form" },
+    { url: "../../../assets/reset-password.png", alt: "Reset Password" },
+    { url: "../../../assets/user-todo.png", alt: "User Todos" },
+    { url: "../../../assets/create-todo.png", alt: "Create Todo" },
+    { url: "../../../assets/edit-todo.png", alt: "Edit Todo" },
+    { url: "../../../assets/delete-todo.png", alt: "Delete Todo" },
+    { url: "../../../assets/users.png", alt: "Users List" },
+    { url: "../../../assets/verify-email-template.png", alt: "Verify Email Template" },
+    { url: "../../../assets/temporary-password-email-template.png", alt: "Temporary Password Email Template" },
+    { url: "../../../assets/reset-password-email-template.png", alt: "Reset Password Email Template" },
+    { url: "../../../assets/login-email-template.png", alt: "Login Email Template" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onImageClick(imageUrl) {
+    swal({
+      content: {
+        element: "img",
+        attributes: {
+          src: imageUrl,
+          style: "max-width: 100%; max-height: 100%"
+        },
+      },
+      buttons: [false],
+    });
+
   }
 
 }
