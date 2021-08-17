4943abb78f3b671c3bd26c09cb4bf68dc7276213
group 28 <hmishaeil@gmail.com>
2021-08-16 18:22:59 -0700

    url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "http://ec2-18-144-41-4.us-west-1.compute.amazonaws.com:8080",
+  apiUrl: "http://ec2-13-52-249-84.us-west-1.compute.amazonaws.com:9001",
   production: true
 };
